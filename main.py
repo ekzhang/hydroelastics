0a07254b6a9f4a5ff8c73967ff494b9484d5db31
Eric Zhang <ekzhang1@gmail.com>
2021-11-04 02:48:26 -0400

    Add Typer skeleton

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world")
+import typer
+
+app = typer.Typer(add_completion=False)
+
+
+@app.command()
+def hello(name: str):
+    """Prints a hello message."""
+    typer.echo(f"Hello {name}")
+
+
+@app.command()
+def spring_pendulum():
+    """Runs a simple spring-pendulum physics simulation."""
+    raise NotImplementedError()
+
+
+if __name__ == "__main__":
+    app()
