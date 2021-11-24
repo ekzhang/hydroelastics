12e0a94420765a9932ef54166d059b0e50be58ab
Eric Zhang <ekzhang1@gmail.com>
2021-11-23 22:44:08 -0500

    Start working on a Julia project (#7)
    * Initial commit creating a Julia project
    
    * Start writing package and add MeshCat
    
    * Clarify README
    
    * Tidy up the manifest
    
    * Implemented Tetrahedron Intersection,
    
    * Made coordinates all (dim x npoints)
    
    * Formatted new object
    
    * Julia com and pressure implementations
    
    * Example of com test
    
    * Add new Manifest.toml and Project.toml
    
    * Resolve merge conflicts
    
    * Nominally refactor tests
    
    * Tests for pressure and triangulation
    
    * More minor changers
    
    * Add typings to function arguments
    
    * Formatting
    
    * Add the Revise.jl package for hot compilation
    
    * Fix tests
    
    * Remove MeshTest stuff
    
    * Added fixes
    
    * Damage control
    
    * Formatting
    
    * Added soft-check for choosing projection.
    
    * Fix some type inference stuff and add back test
    
    Co-authored-by: Franklyn Wang <franklynw2000@gmail.com>
    Co-authored-by: Vincent Huang <vincenthuang75025@yahoo.com>

--- README.md
+++ README.md
@@ -1,43 +1,41 @@
-# Hydroelastics
+# Hydroelastics.jl
 
 This repository contains code for a contact force simulator for soft objects, using a hydroelastic intersection model.
 
-The simulator is differentiable and implemented using [Taichi](https://taichi.graphics/).
+The simulator is differentiable and implemented using the [Julia](https://julialang.org/) programming language.
 
 ## Setup
 
 The following requirements must be present in your environment.
 
-- [Python 3.9](https://python.org/) and [Pip](https://pypi.org/project/pip/)
+- [Julia 1.6+](https://julialang.org/)
 - [Just](https://github.com/casey/just): a command runner
-- GPU drivers for CUDA, OpenGL, Metal, or Vulkan
 
-Then, run the following command to install Python dependencies, including the Taichi runtime.
+Then, run the following command to instantiate the Julia package environment.
 
 ```bash
 just install
 ```
 
-To run a top-level Python entry point in `main.py`, use the command:
+To run the interactive [Julia REPL](https://docs.julialang.org/en/v1/stdlib/REPL/) or open a [Pluto](https://github.com/fonsp/Pluto.jl) notebook server, use the following commands:
 
 ```bash
-just run [COMMAND]
+just repl      # open Julia REPL (can add packages here)
+just notebook  # start server (try opening examples/cube.jl)
 ```
 
-To run unit tests specified in the code, use the command:
+To unit tests specified in the `test/` folder, use the command:
 
 ```bash
 just test
 ```
 
-You can use `just run --help` to display a list of runnable commands. Each program may take appropriate command-line arguments (parsed via [Typer](https://typer.tiangolo.com/)) to customize their behavior.
-
 ## Development
 
-We use [Black](https://black.readthedocs.io/en/stable/) to automatically format Python code. To format all of the files in the project, you can run the command:
+We use [JuliaFormatter.jl](https://github.com/domluna/JuliaFormatter.jl) to automatically format Python code. To format all of the files in the project, you can run the command:
 
 ```bash
 just format
 ```
 
-Note that there is a CI job that automatically checks formatting (and hopefully in the future, test cases) before pull requests can be merged.
+Note that there is a CI job that automatically checks formatting and test cases before pull requests can be merged.
---
+++ test/runtests.jl
@@ -0,0 +1,141 @@
+using Test
+using Hydroelastics
+using LinearAlgebra
+
+function test_isect_tet()
+    tet1 = Mesh(
+        [
+            0.0 1.0 0.0 0.0
+            0.0 0.0 1.0 0.0
+            0.0 0.0 0.0 1.0
+        ],
+        reshape([1, 2, 3, 4], :, 1),
+        [0.0, 0.0, 0.0, 1.0],
+    )
+    tet2 = Mesh(
+        [
+            0.0 1.0 0.0 0.0
+            0.0 0.0 1.0 0.0
+            1.8 1.8 1.8 0.8
+        ],
+        reshape([1, 2, 3, 4], :, 1),
+        [0.0, 0.0, 0.0, 1.0],
+    )
+
+    points = intersect_tets(tet1, tet2, 1, 1)
+    expected_points = [[0.0, 0.0, 0.9], [0.1, 0.0, 0.9], [0.0, 0.1, 0.9]]
+
+    for p in eachcol(points)
+        # there must exist a j in res which is close
+        found = false
+        for q in expected_points
+            if norm(p - q) < 1e-4
+                found = true
+            end
+        end
+        if !found
+            error("could not find returned point $p in $expected_points")
+        end
+    end
+
+    true
+end
+
+@test test_isect_tet()
+
+function test_com()
+    tet = Mesh(
+        [
+            0.0 3 0 0 2
+            0 0 0 6 4
+            0 0 9 0 6
+        ],
+        [
+            1 2
+            2 3
+            3 4
+            4 5
+        ],
+        [0.0, 0.0, 0.0, 0.0, 0.0],
+    )
+
+    @test norm(tet.com - [1.0, 2.0, 3.0]) < 1e-6
+end
+
+test_com()
+
+function test_triangulation()
+    # start with (0, 0), (1, 0), (1, 1), (0, 2), (-2, 2), (-2, 1)
+    # map x, y -> 2*x+1, x-y-2, 2*y-x
+    # then shuffle vertices; old:new mapping is {0:0, 1:4, 2:1, 3:3, 4:5, 5:2}
+    polygon = [
+        1.0 3 -3 1 3 -3
+        -2 -2 -5 -4 -1 -6
+        0 1 4 4 -1 6
+    ]
+    result = [
+        1 1 1 1
+        3 6 4 2
+        6 4 2 5
+    ]
+    @test triangulate_polygon(polygon) == result
+end
+
+test_triangulation()
+
+function test_force()
+    tet1 = Mesh(
+        [
+            0.0 1 0 0
+            0 0 1 0
+            0 0 0 1
+        ],
+        reshape([1, 2, 3, 4], 4, 1),
+        [0.0, 0.0, 0.0, 1.0],
+    )
+    tet2 = Mesh(
+        [
+            0 1 0 0
+            0 0 1 0
+            1.8 1.8 1.8 0.8
+        ],
+        reshape([1, 2, 3, 4], 4, 1),
+        [0.0, 0.0, 0.0, 1.0],
+    )
+
+    force = tet_force(tet1, tet2, 1, 1)
+    # from prev test we know the intersection is a triangle (0,0,.9), (.1, 0, .9), (0, .1, .9)
+    # so the weights on the vtxs of A should be .033, .033, .033, .9
+    expected_force = [0, 0, -0.9]
+    @test norm(force - expected_force) < 1e-6
+end
+
+test_force()
+
+function isect_cubes()
+    function get_cube(com::Vector{Float64})
+        """
+        returns a cube with center at com
+        """
+        cube_verts =
+            com .+ [
+                -1.0 -1.0 -1.0 -1.0 1.0 1.0 1.0 1.0 0.0
+                -1.0 -1.0 1.0 1.0 -1.0 -1.0 1.0 1.0 0.0
+                -1.0 1.0 -1.0 1.0 -1.0 1.0 -1.0 1.0 0.0
+            ]
+        cube_tets = [
+            1 4 1 1 1 1 8 4 8 7 4 8
+            2 2 2 5 3 5 4 3 7 6 8 6
+            3 3 6 6 7 7 7 7 6 5 2 2
+            9 9 9 9 9 9 9 9 9 9 9 9
+        ]
+        cube_pots = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]
+        Mesh(cube_verts, cube_tets, cube_pots)
+    end
+    cu1 = get_cube([0.0, 0.0, 0.0])
+    cu2 = get_cube([0.39103, 0.0232, 0.4312])
+
+    mesh_force(cu1, cu2)
+end
+
+@test isect_cubes() ≈ [-8.45374947, -4.40608135, -4.8481985]
